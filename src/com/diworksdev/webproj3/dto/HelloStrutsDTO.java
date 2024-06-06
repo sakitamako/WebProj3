8afed7ee92bb6addeeb78fb785cc4742b537a8e3
sakitamako <sa.919.1025.ki@gmail.com>
2024-06-06 22:30:29 +0900

    フレームワーク３、P9

---
+++ src/com/diworksdev/webproj3/dto/HelloStrutsDTO.java
@@ -0,0 +1,18 @@
+package com.diworksdev.webproj3.dto;
+
+public class HelloStrutsDTO {
+
+	//テーブルから取得するデータに対応したフィールド変数を宣言
+	private String result;
+
+	//フィールド変数に対応したgetterとsetterを定義
+	public String getResult() {
+		return result;
+	}
+
+	//フィールド変数に対応したgetterとsetterを定義
+	public void setResult(String result) {
+		this.result = result;
+	}
+
+}

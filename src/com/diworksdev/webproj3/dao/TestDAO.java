d45d3f14c7f680e823a0ee23ba2442f1c16b1a40
sakitamako <sa.919.1025.ki@gmail.com>
2024-06-10 21:24:35 +0900

    フレームワーク３、P36

---
+++ src/com/diworksdev/webproj3/dao/TestDAO.java
@@ -0,0 +1,82 @@
+package com.diworksdev.webproj3.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.diworksdev.webproj3.util.DBConnector;
+
+public class TestDAO {
+
+	//①クラス、メソッドの定義
+	//LoginDTO型を最後に呼び出し元に渡すので、LoginDTO型を戻り値にしたメソッドを作る
+	//Actionクラスの値を引数として受け取る
+	public int insert(String username, String password) {
+
+		int ret = 0;
+
+		//②DBConnectorのインスタンス化
+		//DBへの接続準備、DBと会話するためのコード、これでログインできる
+		//Connectionは特定のデータベースとの接続
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		//④sql文を書く：値は ? を入れておく（どんな値でも使いまわしできるようにするため）
+		//usersに入っているデータuser_nameとpasswordに入る条件を満たしたデータがsqlに代入される
+		String sql = "insert into users(user_name, password) values(?,?)";
+
+		//try.catchはjavaの例外処理のための構文
+		try {
+
+			//tryの中にはエラーが発生しそうな処理を書く
+			//⑤PreparedStatement（DBまで運んでくれる箱のイメージ）に代入
+			//定義したSQL文の1番目の?にActionから送られたname、
+			//2番目の?にActionから送られたpasswordがそれぞれ入る
+			PreparedStatement ps = con.prepareStatement(sql);
+
+			//⑥sql文の?に入れる値をsetする
+			ps.setString(1, username);
+			ps.setString(2, password);
+
+			//⑦executeQuery()/executeUpdate()で実行
+			//（select文の場合はexectuteQuery()を使う）
+			// select文のSQL文を実行するメソッドで戻り値はResultSet
+			int i = ps.executeUpdate();
+
+			//もしiが0より大きい場合i件登録されました
+			if (i > 0) {
+				System.out.println(i + "件登録されました");
+
+				ret = i;
+
+			}
+
+		//処理中にSQL関連のエラーが発生した際に実行する処理
+		//tryの中でエラーが発生した場合、catchが受け取り
+		//例外がスローされる原因となったエラーまたは動作の説明を返す
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+
+		//try.catchはjavaの例外処理のための構文
+		try {
+
+			//⑨con.close()で接続を切る
+			//データベースとの接続をクローズ
+			//これをしないとデータベースを接続したまま作業が実行されてしまってメモリに負荷がかかる
+			con.close();
+
+		//処理中にSQL関連のエラーが発生した際に実行する処理
+		//tryの中でエラーが発生した場合、catchが受け取り
+		//例外がスローされる原因となったエラーまたは動作の説明を返す
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+
+		//dtoに入った値を呼び出し元であるActionクラスに渡す
+		return ret;
+	}
+
+}
